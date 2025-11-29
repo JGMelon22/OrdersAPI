47f2a78ce046dcd215970e63bf261820d44e0182
JGMelon22 <jgvasconcellosm22@outlook.com>
2025-11-29 16:42:37 -0300

    feat: adiciona configuração do SQLite

---
+++ src/config/database.js
@@ -0,0 +1,17 @@
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
+const dbPath = path.resolve(__dirname, '../../database.sql');
+
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados:', err.message);
+  } else {
+    console.log('Conectado ao banco de dados SQLite');
+  }
+});
+
+// Habilita foreign keys
+db.run('PRAGMA foreign_keys = ON');
+
+module.exports = db;
